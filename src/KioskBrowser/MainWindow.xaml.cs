9eeefe8918b93ba8fe2066630b84bbfab55742bd
Danielx64 <289220+Danielx64@users.noreply.github.com>
2022-07-20 21:13:58 +1000

    Added function to clean URi strings

--- src/KioskBrowser/MainWindow.xaml.cs
+++ src/KioskBrowser/MainWindow.xaml.cs
@@ -181,4 +181,20 @@ public partial class MainWindow
 	{
 		CloseWindow();
 	}
+
+	public static string RemoveSpecialChars(string str)
+	{
+		// Create  a string array and add the special characters you want to remove
+		string[] chars = new string[] { "kioskbrowser", "~", "`", "!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+", "}", "{", "]", "[", "|", "\"", ":", "'", ":", "?", ">", "<", "/", ".", ",", "\\" };
+
+		//Iterate the number of times based on the String array length.
+		for (int i = 0; i < chars.Length; i++)
+		{
+			if (str.Contains(chars[i]))
+			{
+				str = str.Replace(chars[i], "");
+			}
+		}
+		return str;
+	}
 }
